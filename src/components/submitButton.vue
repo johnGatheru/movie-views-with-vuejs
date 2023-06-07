8ad621252340ca6b36a54147e912c88d50632456
jonny <gatherujohn11@gmail.com>
2023-06-07 16:08:46 +0300

    added a button component for code reusabilty

---
+++ src/components/submitButton.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+defineProps<{
+  text: string;
+}>();
+</script>
+<template>
+  <button
+    @click="$emit('on-clicked', $event)"
+    class="border border-orange-800 mt-1 py-1 px-2 rounded-md"
+  >
+    {{ text }}
+  </button>
+</template>
