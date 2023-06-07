d4426135c3066c1c1a06c6c8d579aad075df7e4e
jonny <gatherujohn11@gmail.com>
2023-06-07 16:11:23 +0300

    added a component to display only a single movie

---
+++ src/components/singleMovie.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+defineProps<{
+  movie: {
+    Poster: string;
+    Title: string;
+    Year: string;
+  };
+}>();
+</script>
+<template>
+  <div class="flex flex-col">
+    <div class="">
+      <img :src="movie.Poster" alt="" class="h-72 object-cover" />
+    </div>
+    <div class="flex flex-col">
+      <h2>{{ movie.Title }}</h2>
+      <div class="flex gap-1">
+        Year: <span>{{ movie.Year }}</span>
+      </div>
+      <p></p>
+    </div>
+  </div>
+</template>
