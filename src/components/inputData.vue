07868a2b1fdd46dd1439f7379494b91ccf7f63c9
jonny <gatherujohn11@gmail.com>
2023-06-07 16:07:57 +0300

    added a custom input for reusability

---
+++ src/components/inputData.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+defineProps<{
+  placeholder: string;
+  modelValue: string | number;
+}>();
+const emit = defineEmits(["update:modelValue"]);
+function takeInput(event: Event) {
+  const target = event.target as HTMLInputElement;
+  emit("update:modelValue", target.value);
+}
+</script>
+<template>
+  <input
+    class="outline-none py-1 px-1 border border-gray-500 rounded-md mt-1 text-gray-600"
+    type="text"
+    :placeholder="placeholder"
+    @input="takeInput($event)"
+    :value="modelValue"
+  />
+</template>
